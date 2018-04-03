1bd647c9d9301c39643ccca811c3cae20839221d
Martin VanWinkle <mvanwinkle@ias.edu>
2018-04-03 13:33:57 -0400

    working on paths.

--- src/lib/python3/IASInfra/IASInfra.py
+++ src/lib/python3/IASInfra/IASInfra.py
@@ -5,6 +5,8 @@ from .IASInfraFullProjectPaths import *
 import os
 import sys
 
+import pprint
+
 class IASInfra(
 	IASInfraLogger,
 	IASInfraDispatcher,
@@ -13,11 +15,30 @@ class IASInfra(
 	def __init__(self, script_name):
 		self.setup_IAS_infra_logging(script_name)
 		
-		self.real_bin=os.path.abspath(os.path.realpath(script_name))
-		self.bin=os.path.abspath(script_name)
+		self.paths={}
+		
+		self.paths['real_bin'] = os.path.dirname(os.path.abspath(os.path.realpath(script_name)))
+		self.paths['bin'] = os.path.dirname(os.path.abspath(script_name))
 	
+		self.default_bin_whence = 'real_bin'
+
+		self.bin_whence = self.default_bin_whence
+		
+		self.are_we_in_src()
 		self.log_debug_variables()
-	
-	def log_debug_variables(self):	
-		self.log_debug("real_bin:" + self.real_bin)
-		self.log_debug("bin: " + self.bin)
+
+	def are_we_in_src(self):
+		self.script_path_components = self.paths[self.bin_whence].split('/')
+		
+		# pp = pprint.PrettyPrinter(indent=4)
+		# pp.pprint(self.script_path_components)
+		# pp.pprint(self.paths)
+		
+		if self.script_path_components[-2] == 'src':
+			return True
+		return False
+		# print("Maybe src dir: " + maybe_src_dir)	
+
+	def log_debug_variables(self):
+		self.log_debug("real_bin:" + self.paths['real_bin'])
+		self.log_debug("bin: " + self.paths['bin'])
--- src/lib/python3/IASInfra/IASInfraFullProjectPaths.py
+++ src/lib/python3/IASInfra/IASInfraFullProjectPaths.py
@@ -1,2 +1,25 @@
+import os
+
 class IASInfraFullProjectPaths:
-	pass
+	def do_base_path_calculations(self):
+		if self.are_we_in_src():
+			self.up_path_components=['..']
+			self.project_name=self.script_path_components[-3]
+		else:
+			self.installed_package_name=self.script_path_components[-2]
+			self.up_path_components=['..','..',self.installed_package_name]
+	
+	def get_generic_project_directory(self, dir_name):
+		if self.are_we_in_src():
+			return os.path.join(
+				self.paths[self.bin_whence],
+				*self.up_path_compoents,
+				dir_name
+			)
+		else:
+			return os.path.join(
+				self.paths[self.bin_whence],
+				*self.up_path_components,
+				dir_name,
+				self.installed_package_name
+			)
